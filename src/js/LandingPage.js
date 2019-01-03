6b9d3f3a5816ea8ba09ebd14716342d43dd8ac9c
Filip Świętek <swietek.filip@gmail.com>
2019-01-03 19:34:13 +0100

    landing page

--- src/js/LandingPage.js
+++ src/js/LandingPage.js
@@ -91,11 +91,119 @@ const CallToAction = () => (
     </div>
 );
 
+const AboutApp = () => {
+    const icons = ['fas fa-check fa-3x', 'far fa-clock fa-3x', 'fas fa-list fa-3x'];
+
+    return (
+        <div className={'container aboutApp'}>
+            {icons.map(icon => (
+                <div key={icon}>
+                    <i className={icon}></i>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat
+                        volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+class NewsletterForm extends React.Component {
+    state = {
+        email: ''
+    };
+
+    handleChange = event => {
+        this.setState({email: event.target.value});
+    };
+
+    render() {
+        return (
+            <div className={'newsletter'}>
+                <div className={'container'}>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <form>
+                        <input onChange={this.handleChange} type='email'/>
+                        <button className={'btn'} type={'submit'}>Lorem</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+const AboutAuthor = () => (
+    <div className={'aboutAuthor container'}>
+        <div className={'pic'}></div>
+        <div>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat.
+                Donec placerat nisl magna, et faucibus arcu condimentum sed. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus
+                arcu condimentum sed.</p>
+        </div>
+    </div>
+);
+
+class Footer extends React.Component {
+    state = {
+        input: ''
+    };
+
+    handleChange = event => {
+        this.setState({email: event.target.value});
+    };
+
+    render() {
+        const icons = ['fab fa-facebook-square fa-2x', 'fab fa-twitter-square fa-2x', 'fab fa-instagram fa-2x'];
+
+        return (
+            <footer>
+                <div className={'container'}>
+                    <div>
+                        <div>
+                            <h2>Lorem ipsum dolor</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat
+                                volutpat. Donec placerat nisl magna.</p>
+                        </div>
+                        <div>
+                            <h2>Lorem ipsum dolor</h2>
+                            <ul>
+                                <li>consectetur adipiscing elit</li>
+                                <li>sed do eiusmod tempor</li>
+                                <li>incididunt ut labore</li>
+                                <li>et dolore magna aliqua</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2>Lorem ipsum dolor</h2>
+                            <form>
+                                <input onChange={this.handleChange} type='email'/>
+                                <button className={'btn'} type={'submit'}>Lorem</button>
+                            </form>
+                            <div className={'socialMedia'}>
+                                {icons.map(icon => (
+                                    <i key={icon} className={icon}></i>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='copyright'>Copyright <span>ZaplanujJedzonko.pl</span></div>
+            </footer>
+        );
+    }
+}
+
 const LandingPage = () => (
     <div className={'main'}>
         <Header/>
         <Slider/>
         <CallToAction/>
+        <AboutApp/>
+        <NewsletterForm/>
+        <AboutAuthor/>
+        <Footer/>
     </div>
 );
 
